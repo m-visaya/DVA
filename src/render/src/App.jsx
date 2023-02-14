d955eae02bcfe5de9af588e43933fb9fadea7789
Marco Visaya <marco.visaya527@gmail.com>
2023-02-14 14:03:51 +0800

    Initial commit

---
+++ src/render/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="bg-palette-gray100 h-screen flex flex-col">
+      <div className="bg-palette-gray75 h-10"></div>
+      <div className="flex justify-center h-full items-center">
+        <h1 className="text-palette-gray25 text-4xl">DVA Dashboard</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/render/tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          gray: "#5A5A5A",
+        },
+        secondary: {
+          gray: "#2D2D2D",
+        },
+        palette: {
+          gray25: "#C2C2C2",
+          gray50: "#909090",
+          gray75: "#2D2D2D",
+          gray100: "#1E1E1E",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
