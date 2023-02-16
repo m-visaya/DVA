f431c4b7e00f9c682c23946330bff35d9367c185
rcbern <rbputil.2sk8l@slmail.me>
2023-02-16 18:09:35 +0800

    Tooltip component

--- src/render/components/tooltip.jsx
+++ src/render/components/tooltip.jsx
@@ -1,4 +1,21 @@
-function tooltip(){
+import liveIcon from "../assets/live.svg"
 
+function tooltip(){
+    return(
+            <div className="2xl:pt-[700px] lg:pt-[500px] md:pt-96">
+                <div className="box-border bg-palette-gray75 lg:h-24 lg:w-72 md:h-16 md:w-56 lg:rounded-3xl md:rounded-2xl">
+                    <div className="flex justify-center h-full items-center">
+                        <div className="grid grid-cols-4 lg:gap-x-4 md:gap-x-3 flex h-full items-center">
+                            <div className="col-span-1">
+                                <img src={liveIcon} className="lg:w-12 md:w-8"></img>  
+                            </div>
+                            <div className="col-span-3">
+                                <p className="text-palette-gray50 font-roboto lg:text-sm md:text-xs md:my-2">View real-time feed from <br/> configured cameras.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
 }
 export default tooltip;
