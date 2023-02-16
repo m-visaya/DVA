80721a6bef3dc67c1ddd900c9f1080e559009390
rcbern <rbputil.2sk8l@slmail.me>
2023-02-16 12:14:34 +0800

    Electron tweaks

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ if (require("electron-squirrel-startup")) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
@@ -25,7 +25,9 @@ const createWindow = () => {
   );
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
+
+  // mainWindow.removeMenu();
 };
 
 // This method will be called when Electron has finished
