c7c560a8f51e163c40cb092617476cd79c82da54
rcbern <rbputil.2sk8l@slmail.me>
2023-02-16 12:14:40 +0800

    Icon tweaks

--- src/render/components/Icon.jsx
+++ src/render/components/Icon.jsx
@@ -1,13 +1,11 @@
-import icon from "../assets/live.svg"
-
-function Icon(){
+function Icon({iconImage, iconTitle}){
  return(
-    <div className="box-border bg-palette-gray75 h-24 w-24 rounded-3xl">
+    <div className="box-border bg-palette-gray75 hover:bg-primary-gray h-24 w-24 rounded-3xl">
       <div className="flex justify-center h-full items-center">
-        <img src={icon} className="w-12"></img>  
+        <img src={iconImage} className="w-12"></img>  
       </div>
       <div className="flex justify-center">
-        <p className="text-palette-gray50 font-roboto text-2xl my-2">Live</p>
+        <p className="text-palette-gray50 font-roboto text-2xl my-2">{iconTitle}</p>
       </div>
     </div>
  );
