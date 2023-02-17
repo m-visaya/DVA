5206519ad4c02a4003a03380cc6a8bbe16e3efda
rcbern <rbputil.2sk8l@slmail.me>
2023-02-17 14:52:55 +0800

    main

--- src/render/src/main.jsx
+++ src/render/src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Dashboard from "./Dashboard";
+import File from "./File";
 import Live from "./Live";
+import Test from "./Test";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Live />
+    <Dashboard />
   </React.StrictMode>
 );
