984b3f46c8a64c91c330b529f3f6561eba371465
rcbern <rbputil.2sk8l@slmail.me>
2023-02-17 09:47:14 +0800

    temporarily points root to live

--- src/render/src/main.jsx
+++ src/render/src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Dashboard from "./dashboard";
+import Dashboard from "./Dashboard";
+import Live from "./Live";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Dashboard />
+    <Live />
   </React.StrictMode>
 );
