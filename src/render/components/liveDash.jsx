0dd8ed6f66d3f74a6fb040a17a42bce817bd8afc
rcbern <rbputil.2sk8l@slmail.me>
2023-02-25 13:29:11 +0800

    Minor UI changes

--- src/render/components/liveDash.jsx
+++ src/render/components/liveDash.jsx
@@ -6,7 +6,7 @@ function liveDash({detectionStatus}) {
   return (
       <div className="absolute bottom-9 left-1/2 transform -translate-x-1/2 box-border bg-palette-gray75 h-auto w-auto lg:rounded-3xl md:rounded-2xl">
         <div className="flex justify-center h-full items-center p-2">
-          <div className="grid grid-cols-3 lg:gap-x-4 h-full items-center">
+          <div className="grid grid-cols-3 gap-x-4 h-full items-center">
             <div>
               <Status 
                 statusTitle={'Camera Status'}
