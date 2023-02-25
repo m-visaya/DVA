e1d08560a22c7185d9e8a0d6a442da6aed22cb34
rcbern <rbputil.2sk8l@slmail.me>
2023-02-25 13:14:08 +0800

    code cleanup

--- src/render/components/liveDash.jsx
+++ src/render/components/liveDash.jsx
@@ -1,15 +1,22 @@
 import Status from "./status";
 import LogButton from "./logButton";
 
-function fileDash({detectionStatus}) {
+
+function liveDash({detectionStatus}) {
   return (
       <div className="absolute bottom-9 left-1/2 transform -translate-x-1/2 box-border bg-palette-gray75 h-auto w-auto lg:rounded-3xl md:rounded-2xl">
         <div className="flex justify-center h-full items-center p-2">
-          <div className="grid grid-cols-2 lg:gap-x-10 md:gap-x-4 h-full items-center ml-3">
+          <div className="grid grid-cols-3 lg:gap-x-10 md:gap-x-4 h-full items-center">
+            <div>
+              <Status 
+                statusTitle={'Camera Status'}
+                statusDescription ={'Connected'}
+              />
+            </div>
             <div>
               <Status 
                 statusTitle={'Detection Status'}
-                statusDescription={detectionStatus}
+                statusDescription ={detectionStatus}
               />
             </div>
             <div>
@@ -20,4 +27,4 @@ function fileDash({detectionStatus}) {
       </div>
   );
 }
-export default fileDash;
+export default liveDash;
