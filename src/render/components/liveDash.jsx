e7e8559a06db2a651d258e3701856446d29dc207
Marco Visaya <marco.visaya527@gmail.com>
2023-02-28 00:33:21 +0800

    Add input select for available video sources, closes #2

--- src/render/components/liveDash.jsx
+++ src/render/components/liveDash.jsx
@@ -1,30 +1,29 @@
 import Status from "./status";
 import LogButton from "./logButton";
 
-
-function liveDash({detectionStatus}) {
+function liveDash({ detectionStatus }) {
   return (
-      <div className="absolute bottom-9 left-1/2 transform -translate-x-1/2 box-border bg-palette-gray75 h-auto w-auto lg:rounded-3xl md:rounded-2xl">
-        <div className="flex justify-center h-full items-center p-2">
-          <div className="grid grid-cols-3 gap-x-4 h-full items-center">
-            <div>
-              <Status 
-                statusTitle={'Camera Status'}
-                statusDescription ={'Connected'}
-              />
-            </div>
-            <div>
-              <Status 
-                statusTitle={'Detection Status'}
-                statusDescription ={detectionStatus}
-              />
-            </div>
-            <div>
-              <LogButton />
-            </div>
+    <div className="absolute bottom-9 left-1/2 transform -translate-x-1/2 box-border bg-palette-gray75 h-auto w-auto lg:rounded-3xl md:rounded-2xl ">
+      <div className="flex justify-center h-full items-center p-2">
+        <div className="grid grid-cols-3 gap-x-4 h-full items-center">
+          <div>
+            <Status
+              statusTitle={"Camera Status"}
+              statusDescription={"Connected"}
+            />
+          </div>
+          <div>
+            <Status
+              statusTitle={"Detection Status"}
+              statusDescription={detectionStatus}
+            />
+          </div>
+          <div>
+            <LogButton />
           </div>
         </div>
       </div>
+    </div>
   );
 }
 export default liveDash;
--- src/render/src/Live.jsx
+++ src/render/src/Live.jsx
@@ -7,81 +7,91 @@ import "@tensorflow/tfjs-backend-webgl";
 
 const MODEL_PATH = "./assets/model/xception_js/model.json";
 
-function live(){
+function live() {
+  const webcamRef = useRef(null);
+  const [prediction, setPrediction] = useState("No Accident Detected");
+  const [model, setModel] = useState(null);
+  const [devices, setDevices] = useState([]);
+  const [device, setDevice] = useState();
 
-    const webcamRef = useRef(null);
-    const [prediction, setPrediction] = useState("No Accident Detected");
-    const [model, setModel] = useState(null);
-    const [devices, setDevices] = useState([]);
+  const handleDevices = useCallback(
+    (mediaDevices) =>
+      setDevices(mediaDevices.filter(({ kind }) => kind === "videoinput")),
+    [setDevices]
+  );
 
-    const handleDevices = useCallback(
-        (mediaDevices) =>
-        setDevices(mediaDevices.filter(({ kind }) => kind === "videoinput")),
-        [setDevices]
-    );
+  useEffect(() => {
+    navigator.mediaDevices.enumerateDevices().then(handleDevices);
+    setDevice(devices[0]?.deviceId);
+  }, [handleDevices]);
 
-    useEffect(() => {
-        navigator.mediaDevices.enumerateDevices().then(handleDevices);
-    }, [handleDevices]);
+  useEffect(() => {
+    const loadModel = async () => {
+      const model = await tf.loadGraphModel(MODEL_PATH);
+      setModel(model);
+      console.log("model loaded");
+    };
+    loadModel();
+  }, []);
 
-    useEffect(() => {
-        const loadModel = async () => {
-        const model = await tf.loadGraphModel(MODEL_PATH);
-        setModel(model);
-        console.log("model loaded");
-        };
-        loadModel();
-    }, []);
+  useEffect(() => {
+    const predict = async () => {
+      if (
+        model &&
+        webcamRef.current &&
+        webcamRef.current.video.readyState === 4
+      ) {
+        try {
+          const img = webcamRef.current.video;
+          const tensor = tf.browser.fromPixels(img);
+          const resized = tf.image.resizeBilinear(tensor, [299, 299]);
+          const reshaped = resized.expandDims(0);
+          let prediction = model.predict(reshaped).arraySync()[0];
+          let conf = prediction * 100;
+          if (prediction[0] * 100 < 50) {
+            prediction = "No Accident Detected";
+          } else {
+            prediction = "Accident Detected";
+            // window.electronAPI.addLog();
+            // window.electronAPI.getLogs();
+            // window.electronAPI.onLogsData((event, rows) => {
+            //   console.log("Contents of the logs table:");
+            //   console.log(rows);
+            //   // Do something with the retrieved rows
+            // });
+          }
+          // prediction = prediction[0] * 100 < 50 ? "No Accident Detected" : "Accident Detected";
 
-    useEffect(() => {
-        const predict = async () => {
-        if (
-            model &&
-            webcamRef.current &&
-            webcamRef.current.video.readyState === 4
-        ) {
-            const img = webcamRef.current.video;
-            const tensor = tf.browser.fromPixels(img);
-            const resized = tf.image.resizeBilinear(tensor, [299, 299]);
-            const reshaped = resized.expandDims(0);
-            let prediction = model.predict(reshaped).arraySync()[0];
-            
-            if (prediction[0] * 100 < 50) {
-                prediction = "No Accident Detected";
-            } else {
-                prediction = "Accident Detected";
-                window.electronAPI.addLog();
-                window.electronAPI.getLogs();
-                window.electronAPI.onLogsData((event, rows) => {
-                  console.log("Contents of the logs table:");
-                  console.log(rows);
-                  // Do something with the retrieved rows
-                });
-            }
-            // prediction = prediction[0] * 100 < 50 ? "No Accident Detected" : "Accident Detected";
-            
-            setPrediction(prediction);
-            console.log(prediction);
-        }
-        requestAnimationFrame(predict);
-        };
-        predict();
+          setPrediction(prediction);
+          console.log(prediction, conf);
+        } catch (error) {}
+      }
+      requestAnimationFrame(predict);
+    };
+    predict();
   }, [webcamRef, model]);
 
-    return(
-        <div className="bg-black h-screen flex flex-col relative">
-        <Webcam
+  return (
+    <div className="bg-black h-screen flex flex-col relative">
+      <div className="ml-auto z-10">
+        <label className="text-white mr-3">Camera Source</label>
+        <select value={device} onChange={(e) => setDevice(e.target.value)}>
+          {devices.map((option) => (
+            <option value={option.deviceId}>{option.label}</option>
+          ))}
+        </select>
+      </div>
+      <Webcam
         className="absolute h-full w-full"
         ref={webcamRef}
-        videoConstraints={{ deviceId: devices[2]?.deviceId }}
-        />
-        <div className="absolute z-10 place-items-start 2xl:pl-10 lg:pl-8 md:pl-6 mt-5">
-            <ReturnButton returnTitle="Live" to="/" />
-        </div>
-        <LiveDash 
-            detectionStatus={prediction}    
-        />
+        videoConstraints={{ deviceId: device }}
+      />
+      <div className="absolute z-10 place-items-start 2xl:pl-10 lg:pl-8 md:pl-6 mt-5">
+        <ReturnButton returnTitle="Live" to="/" />
       </div>
-    );
+
+      <LiveDash detectionStatus={prediction} />
+    </div>
+  );
 }
 export default live;
