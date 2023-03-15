4f1820bec92939b07305562f83849a78184064bb
rcbern <rbcon.ks77m@slmail.me>
2023-03-15 17:49:22 +0800

    dashboard 0.2.0

--- src/render/components/dashboard/Icon.jsx
+++ src/render/components/dashboard/Icon.jsx
@@ -1,11 +1,14 @@
-function icon({iconImage, iconTitle}){
+function icon({iconImage, iconTitle, iconDescription}){
  return(
-    <div className="box-border bg-palette-gray75 hover:bg-primary-gray h-14 w-14 rounded-2xl">
-      <div className="flex justify-center h-full items-center">
-        <img src={iconImage} className="w-8"></img>  
-      </div>
-      <div className="flex justify-center">
-        <p className="text-palette-gray50 font-roboto my-2">{iconTitle}</p>
+    <div className="box-border bg-palette-gray75 hover:bg-primary-gray h-auto w-64 rounded-2xl drop-shadow-lg">
+      <div className="grid grid-cols-3 flex items-center h-full py-4">
+        <div className="flex justify-center">
+          <img src={iconImage} className="w-7"></img>  
+        </div>
+        <div className="col-span-2 mr-3">
+          <p className="text-palette-gray50 font-bold font-roboto">{iconTitle}</p>
+          <p className="text-palette-gray50 text-[8pt] font-roboto">{iconDescription}</p>
+        </div>
       </div>
     </div>
  );
--- src/render/src/Dashboard.jsx
+++ src/render/src/Dashboard.jsx
@@ -1,49 +1,57 @@
 import { Link } from "react-router-dom";
-import Icon from "../components/dashboard/icon";
-import liveIcon from "../assets/graphics/live.svg";
-import fileIcon from "../assets/graphics/file.svg";
 import configureIcon from "../assets/graphics/configure.svg";
-import exitIcon from "../assets/graphics/exit.svg";
+// import exitIcon from "../assets/graphics/exit.svg";
+import fileIcon from "../assets/graphics/file.svg";
+import liveIcon from "../assets/graphics/live.svg";
 import logIcon from "../assets/graphics/logs.svg";
+import Icon from "../components/dashboard/icon";
 
 function Dashboard() {
   return (
     <div className="bg-palette-gray100 h-screen flex flex-col">
       <div className="flex justify-center h-full items-center">
-        <div className="grid grid-cols-5 lg:gap-x-14 md:gap-x-8">
+        <div className="grid grid-cols-2 gap-6">
           <div>
             <Link to="/live">
               <Icon 
               iconImage={liveIcon} 
-              iconTitle="Live" />
+              iconTitle="Live"
+              iconDescription="Detect vehicular accidents in live camera streams."
+              />
             </Link>
           </div>
           <div>
             <Link to="/file">
               <Icon 
               iconImage={fileIcon} 
-              iconTitle="File" />
+              iconTitle="File" 
+              iconDescription="Detect vehicular accidents in media files."
+              />
             </Link>
           </div>
           <div>
             <Link to="/logs">
               <Icon 
               iconImage={logIcon} 
-              iconTitle="Logs" />
+              iconTitle="Logs" 
+              iconDescription="Review logged detection of vehicular accident."
+              />
             </Link>
           </div>
           <div>
             <Link to="/config">
               <Icon 
               iconImage={configureIcon} 
-              iconTitle="Configure" />
+              iconTitle="Configure" 
+              iconDescription="Configure various application settings."
+              />
             </Link>
           </div>
-          <div>
+          {/* <div>
             <Icon 
             iconImage={exitIcon} 
             iconTitle="Exit" />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
