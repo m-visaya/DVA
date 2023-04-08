5ed25c17e48a393348a6762b9c11b6b6564ee625
rcbern <rbputil.2sk8l@slmail.me>
2023-04-08 16:47:00 +0800

    Log item modal

--- src/render/src/Preview.jsx
+++ src/render/src/Preview.jsx
@@ -1,46 +1,37 @@
+import React from "react";
+import ReactSwipe from "react-swipe";
+
 function preview(){
+
+    let reactSwipeEl;
+
     return(
       <div className="bg-palette-white75 dark:bg-palette-gray100 min-h-screen flex flex-col">
-        <div
-          id="carouselExampleControls"
-          className="relative"
-          data-te-carousel-init
-          data-te-carousel-slide>
-          <div
-            className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
-            <div
-              className="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-              data-te-carousel-item
-              data-te-carousel-active>
-              <img
-                src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
-                className="block w-full"
-                alt="Wild Landscape" />
-            </div>
-            <div
-              className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-              data-te-carousel-item>
-              <img
-                src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
-                className="block w-full"
-                alt="Camera" />
-            </div>
-            <div
-              className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-              data-te-carousel-item>
-              <img
-                src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
-                className="block w-full"
-                alt="Exotic Fruits" />
-            </div>
-          </div>
-
-          {/* Carousel controls - prev item */}
+        <div clasName="flex h-full m-24">
+            <ReactSwipe
+              className="carousel"
+              swipeOptions={{ continuous: false }}
+              ref={el => (reactSwipeEl = el)}
+            >
+              <div> 
+                {/* loop this div block */}
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
+                  className="object-cover object-bottom"></img>
+              </div>
+              <div
+              >
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
+                  className="object-fill"></img>
+              </div>
+            </ReactSwipe>
+        </div>
+        <div className="z-10">
           <button
-            className="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+            className="absolute bottom-0 left-0 top-0 flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
             type="button"
-            data-te-target="#carouselExampleControls"
-            data-te-slide="prev">
+            onClick={() => reactSwipeEl.prev()}>
             <span className="inline-block h-8 w-8">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -48,24 +39,18 @@ function preview(){
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
-                className="h-6 w-6">
+                className="h-8 w-8">
                 <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
                   d="M15.75 19.5L8.25 12l7.5-7.5" />
               </svg>
             </span>
-            <span
-              className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-              >Previous</span
-            >
           </button>
-          {/*Carousel controls - next item*/}
           <button
-            className="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
+            className="absolute bottom-0 right-0 top-0 flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
             type="button"
-            data-te-target="#carouselExampleControls"
-            data-te-slide="next">
+            onClick={() => reactSwipeEl.next()}>
             <span className="inline-block h-8 w-8">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -73,18 +58,55 @@ function preview(){
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
-                className="h-6 w-6">
+                className="h-8 w-8">
                 <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
                   d="M8.25 4.5l7.5 7.5-7.5 7.5" />
               </svg>
             </span>
-            <span
-              className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-              >Next</span
-            >
           </button>
+        </div>
+        <div className="z-20">
+          <div className="absolute bottom-9 left-1/2 transform -translate-x-1/2 box-border drop-shadow-lg bg-palette-white75 dark:bg-palette-gray75 h-auto w-3/4 rounded-xl">
+            <div className="grid grid-cols-3 grid-rows-2 py-3 px-5 gap-y-3">
+              <div>
+                <p className="text-palette-gray75 dark:text-palette-gray50 font-bold font-roboto text-[10pt] ">
+                  Type :
+                </p>
+                <p className="text-palette-gray75 dark:text-palette-gray50 font-roboto text-[8pt]">
+                  Live Capture
+                </p>
+              </div>
+              <div>
+                <p className="text-palette-gray75 dark:text-palette-gray50 font-bold font-roboto text-[10pt] ">
+                  Origin :
+                </p>
+                <p className="text-palette-gray75 dark:text-palette-gray50 font-roboto text-[8pt]">
+                  OBS Virtual Camera
+                </p>
+              </div>
+              <div>
+                <p className="text-palette-gray75 dark:text-palette-gray50 font-bold font-roboto text-[10pt] ">
+                  Date Occured :
+                </p>
+                <p className="text-palette-gray75 dark:text-palette-gray50 font-roboto text-[8pt]">
+                  4:39:25 04/09/2023
+                </p>
+              </div>
+              <div className="col-span-2">
+                <p className="text-palette-gray75 dark:text-palette-gray50 font-bold font-roboto text-[10pt] ">
+                  File Path :
+                </p>
+                <p className="text-palette-gray75 dark:text-palette-gray50 font-roboto text-[8pt]">
+                  C:/Users/Administrator/Documents/DVA/saved/accident001
+                </p>
+              </div>
+              <div className="flex justify-center items-center bg-primary-blue dark:bg-palette-gray100 ease-in duration-200 hover:bg-palette-gray25 dark:hover:bg-primary-gray rounded-xl cursor-pointer">
+                <p className="text-white dark:text-palette-gray50 font-roboto text-[10pt]">Open Directory</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
