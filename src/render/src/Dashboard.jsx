97c5ccae4daba22b37e1dbcc667def04df801f76
rcbern <rbputil.2sk8l@slmail.me>
2023-04-09 15:51:02 +0800

    Help button theme adaptive.

--- src/render/src/Dashboard.jsx
+++ src/render/src/Dashboard.jsx
@@ -44,15 +44,17 @@ function Dashboard() {
                         ease-in-out duration-200 hover:bg-palette-white25 hover:dark:bg-palette-gray80
                         font-roboto font-bold text-[10pt]
                         p-2 rounded-xl flex grid grid-cols-2 place-items-center z-20">
-          <img src={HelpIcon} className="w-5"></img>
-          <p className="mr-1">Help</p>              
+          <svg width="21" height="21" viewBox="0 0 21 21" strokeWidth={1.5} className="fill-palette-gray75 dark:fill-palette-gray50" xmlns="http://www.w3.org/2000/svg">
+          <path d="M9.45 16.8H11.55V14.7H9.45V16.8ZM10.5 0C4.704 0 0 4.704 0 10.5C0 16.296 4.704 21 10.5 21C16.296 21 21 16.296 21 10.5C21 4.704 16.296 0 10.5 0ZM10.5 18.9C5.8695 18.9 2.1 15.1305 2.1 10.5C2.1 5.8695 5.8695 2.1 10.5 2.1C15.1305 2.1 18.9 5.8695 18.9 10.5C18.9 15.1305 15.1305 18.9 10.5 18.9ZM10.5 4.2C8.1795 4.2 6.3 6.0795 6.3 8.4H8.4C8.4 7.245 9.345 6.3 10.5 6.3C11.655 6.3 12.6 7.245 12.6 8.4C12.6 10.5 9.45 10.2375 9.45 13.65H11.55C11.55 11.2875 14.7 11.025 14.7 8.4C14.7 6.0795 12.8205 4.2 10.5 4.2Z"/>
+          </svg>
+          <p className="mr-1 text-palette-gray75 dark:text-palette-gray50">Help</p>              
         </div>
 
       </Link>
-      <img
+      {/* <img
         src="../assets/graphics/background.svg"
         className="absolute w-full h-full object-cover object-bottom opacity-80 overflow-hidden"
-      ></img>
+      ></img> */}
       <div className="flex justify-center h-full items-center z-10">
         <div className="w-auto h-auto p-12 rounded-xl">
           <div className="grid grid-cols-2 gap-6 xl:gap-y-12 xl:gap-x-24">
