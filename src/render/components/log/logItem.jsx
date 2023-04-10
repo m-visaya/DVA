541d4d4733358c6b5c138d03e930331bb39831be
rcbern <rbputil.2sk8l@slmail.me>
2023-04-10 10:05:59 +0800

    Log module visual fixes

--- src/render/components/log/logItem.jsx
+++ src/render/components/log/logItem.jsx
@@ -17,7 +17,7 @@ function logItem({ LogID, LogType, LogOrigin, LogDate, LogPath }) {
 
     return (
         <div className="flex flex justify-center" onClick={() => window.electronAPI.openLog(LogID)}>
-            <div className="grid grid-cols-5 gap-x-10 md:w-3/4 lg:w-[800px] py-2 border-b border-palette-gray75">
+            <div className="grid grid-cols-5 gap-x-10 md:w-3/4 lg:w-[800px] py-2 border-b border-palette-gray-25 dark:border-palette-gray75">
                 <div className="text-palette-gray50 font-roboto lg:text-[10pt] md:text-[8pt] flex items-center truncate">{LogType}</div>
                 <div className="text-palette-gray50 font-roboto lg:text-[10pt] md:text-[8pt] flex items-center truncate">{LogOrigin}</div>
                 <div className="text-palette-gray50 font-roboto lg:text-[10pt] md:text-[8pt] flex items-center truncate">{LogDate}</div>
