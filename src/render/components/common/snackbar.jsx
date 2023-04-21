0e2611189ea950d6ad8c85938342d052f842ac59
rcbern <rbputil.2sk8l@slmail.me>
2023-04-21 14:59:44 +0800

    Added animation to snackbar.

--- src/render/components/common/snackbar.jsx
+++ src/render/components/common/snackbar.jsx
@@ -4,13 +4,13 @@ function Snackbar(props) {
   useEffect(() => {
     const timer = setTimeout(() => {
       props.setSnackVisible(false);
-    }, 2000);
+    }, 3000);
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <div className="absolute bottom-0 right-0 m-4 place rounded-xl bg-sky-100 text-sky-800 text-[10pt] px-6 py-4 z-10">
+    <div className="absolute bottom-0 right-0 m-4 place rounded-xl bg-sky-100 text-sky-800 text-[10pt] px-6 py-4 z-10 elementToFadeInAndOut">
       { props.text }
     </div>
   );
